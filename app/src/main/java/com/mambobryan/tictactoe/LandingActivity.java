f836ade8ea5eda11a77831d0dfd51acdc291fcf8
MamboBryan <mambobryan@gmail.com>
2019-04-03 23:01:34 +0300

    [Feature #165100178] Added mainActivity

--- app/src/main/java/com/mambobryan/tictactoe/LandingActivity.java
+++ app/src/main/java/com/mambobryan/tictactoe/LandingActivity.java
@@ -1,13 +1,32 @@
 package com.mambobryan.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingActivity extends AppCompatActivity {
+
+    //Declaring the playButton variable
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        //Initializing the play button variable to the xml button
+        playButton = findViewById(R.id.play_button);
+
+        //Setting an onClick listener for the button to got to mainActivity
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creating an intent object
+                Intent landingIntent = new Intent(LandingActivity.this, MainActivity.class);
+                LandingActivity.this.startActivity(landingIntent);
+            }
+        });
     }
 }
